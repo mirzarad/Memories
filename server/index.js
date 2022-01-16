3317645a134b789dfe8bcd987d284d2ea4f7ca29
mirzarad <mirza.radoncic@stonybrook.edu>
2022-01-15 21:23:40 -0500

    Setup env variable configuration

--- server/index.js
+++ server/index.js
@@ -2,9 +2,11 @@ import express from 'express'
 import bodyParser from 'body-parser'
 import mongoose from 'mongoose'
 import cors from 'cors'
+import dotenv from 'dotenv'
 import postRoutes from './routes/posts.js';
 
 const app = express();
+dotenv.config();
 
 // localhost:5000/posts
 // every route from the postRoutes will have a directory prefix of /posts
@@ -21,11 +23,9 @@ app.use('/posts', postRoutes);
     This is the cloud version of mongo where they will be hosting the db for our application.
 */
 
-const CONNECTION_URL = 'mongodb+srv://<username>:<password>!@cluster0.b4k0e.mongodb.net/myFirstDatabase?retryWrites=true&w=majority'
-
 const PORT = process.env.PORT || 5000;
 
-mongoose.connect(CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
+mongoose.connect(process.env.CONNECTION_URL, {useNewUrlParser: true, useUnifiedTopology: true})
   .then(() => app.listen(PORT, () => console.log(`Server running on port: ${PORT}`)))
   .catch((error) => console.log(error.message));
 
